3331f80d2326b5fa64eca328fe7f65236f8732df
donghyun <pointehd@gmail.com>
2019-10-26 19:09:53 +0900

    훅테스트

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,7 +1,7 @@
 <template>
   <DefaultLayout>
     <div class="content_de">
-      <div>{{markList.title}} 으어어ㅓㄱ</div>
+      <div>{{markList.title}} 으어어ㅓㄱㅋㅋㅋ</div>
       <div class="mark_content" v-html="markDown"></div>
       <div>{{markList.date}}</div>
     </div>
